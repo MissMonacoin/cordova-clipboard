f969148528cab5a3461076e57786f46c0eeb881c
あ <>
2018-01-29 20:13:23 +0900

    Added Browser

---
+++ src/browser/clipboardProxy.js
@@ -0,0 +1,26 @@
+require("cordova/exec/proxy").add("Clipboard", {
+  copy:function(win,fail,args){
+    var data = args[0];
+    var temp = document.createElement('textarea');
+    temp.setAttribute('readonly', '')
+    temp.textContent = data;
+    
+    var s = temp.style;
+    s.position = 'absolute';
+    s.border="0";
+    s.padding="0";
+    s.margin="0";
+    s.left = '-999px';
+    s.top=(window.pageYOffset || document.documentElement.scrollTop)+"px"
+
+    document.body.appendChild(temp);
+    temp.setSelectionRange(0, temp.value.length);
+
+    var result = document.execCommand('copy');
+
+    document.body.removeChild(temp);
+  },
+  paste:function(win,fail){
+    console.log("Paste is not supported in browser")
+  }
+});
